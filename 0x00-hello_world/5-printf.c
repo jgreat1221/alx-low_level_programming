0ce12592cdff7fa02a778e4a989a9e7f98ec7b17
jgreat1221 <niyi4279@gmail.com>
2023-09-07 12:17:05 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  Hello, printf
+ * Return: o (Success)
+ */
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,\n")
+	return (o);
+}
