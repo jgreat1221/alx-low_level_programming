6b7d28fa23d6279efd7b2009104975b3b5b8711d
jgreat1221 <niyi4279@gmail.com>
2023-09-07 22:13:25 -0700

    QUOTE

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A cprogram that print a line to the standard erro
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+	return (1);
+}
