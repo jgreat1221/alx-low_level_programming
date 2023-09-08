2e5423f1e5cf8efc096579b2775fc4cf29140e32
jgreat1221 <niyi4279@gmail.com>
2023-09-07 21:15:36 -0700

    piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - Entry point
  * Return: 0 (Success)
+ *
+ *
  */
 int main(void)
 {
-pritf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
