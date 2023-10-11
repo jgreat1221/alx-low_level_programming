c2276ac73f4507bfd02c5bfba1a7f35994391560
jgreat1221 <niyi4279@gmail.com>
2023-10-11 01:18:19 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @age: age of dog
+ * @name: name of dog
+ * @owner: name of owner
+ */
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
