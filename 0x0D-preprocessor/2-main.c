e7ff6ae442fcbfe8120f2ba95813b2e7bf5b2b97
jgreat1221 <niyi4279@gmail.com>
2023-10-11 04:23:48 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that print the name of the file
+ * it was complied from
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
